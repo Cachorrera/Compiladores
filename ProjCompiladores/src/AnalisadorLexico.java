cbe64a07865923d0cc7c6573ec494379eca64fc4
Cachorrera <fernandohnb@gmail.com>
2017-08-02 16:39:07 -0300

    Update AnalisadorLexico.java
    Adicionado reconhecimento de operadores.

--- ProjCompiladores/src/AnalisadorLexico.java
+++ ProjCompiladores/src/AnalisadorLexico.java
@@ -6,7 +6,6 @@ public class AnalisadorLexico {
 	public static final int S1 = 1;
 	public static final int S2 = 2;
 	public static final int S3 = 3;
-	public static final int S5 = 5;
 	public static final int SL = 4;
 
 	private String fileContent;
@@ -26,6 +25,7 @@ public class AnalisadorLexico {
 			String linha;
 			StringBuilder fContent = new StringBuilder();
 			while ((linha = br.readLine()) != null){
+				linha+='\n';
 				fContent.append(linha);
 			}
 			br.close();
@@ -47,17 +47,18 @@ public class AnalisadorLexico {
 	private boolean isDigit(char ch){
 		return (ch >= '0' && ch <= '9');
 	}
-
 	private boolean isAlpha(char ch){
 		return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
 	}
 	private boolean isSpace(char ch){
 		return (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r');
 	}
+	private boolean isOper(char ch){
+		return (ch == '+' || ch == '-' || ch == '*' || ch == '/' );
+	}
 	public void retroceder(){
 		index--;
 	}
-
 	public boolean isEOF(){
 		return (index == fileContent.length());
 	}
@@ -93,7 +94,14 @@ public class AnalisadorLexico {
 				}
 				else if (isAlpha(c)){
 					symbol+=c;
-					estado = S5;
+					estado = S3;
+				}
+				else if (isOper(c)){
+					symbol+=c;
+					token = new Token();
+					token.setCode(Token.OPERADOR);
+					token.setSymbol(symbol);
+					return token;
 				}
 				else{
 					estado = SL;
@@ -101,7 +109,7 @@ public class AnalisadorLexico {
 				break;
 			case S1:
 				c = nextChar();
-
+				
 				if (isDigit(c)){
 					symbol+=c;
 					estado = S1;
@@ -110,25 +118,30 @@ public class AnalisadorLexico {
 					symbol+=c;
 					estado = S2;
 				}
-				else if (isAlpha(c)){
-					estado = SL;
-				}
-				else if(isSpace(c)){
+				else if(isSpace(c) || isOper(c)){
 					token = new Token();
 					token.setCode(Token.NUMERO_INTEIRO);
 					token.setSymbol(symbol);
 					retroceder();
 					return token;
-				}else{
+				}
+				else{
 					estado = SL;
 				}
 				break;
 			case S2:
 				c = nextChar();
-
+				
 				if (isDigit(c)){
 					symbol+=c;
-					estado = S3;
+					estado = S2;
+				}
+				else if(isSpace(c) || isOper(c)){
+					token = new Token();
+					token.setCode(Token.NUMERO_PONTO_FIXO);
+					token.setSymbol(symbol);
+					retroceder();
+					return token;
 				}
 				else{
 					estado = SL;
@@ -136,36 +149,23 @@ public class AnalisadorLexico {
 				break;
 			case S3:
 				c = nextChar();
-
-				if (isDigit(c)){
+				
+				if (isAlpha(c)){
 					symbol+=c;
 					estado = S3;
-				}else if(c == '.'){
-					estado = SL;
 				}
-				else{
-					token = new Token();
-					token.setCode(Token.NUMERO_PONTO_FIXO);
-					token.setSymbol(symbol);
-					retroceder();
-					return token;
+				else if (isDigit(c)){
+					symbol+=c;
+					estado = S3;   
 				}
-				break;
-			case S5:
-				c = nextChar();
-
-				if (isDigit(c)){
-					symbol+=c;
-					estado = S5;
-				}else if(isAlpha(c)){
-					symbol+=c;
-					estado = S5;
-				}else if(isSpace(c)){
+				else if(isSpace(c)){
 					token = new Token();
 					token.setCode(Token.IDENTIFICADOR);
 					token.setSymbol(symbol);
 					retroceder();
 					return token;
+				}else{
+					estado = SL;
 				}
 				break;
 			case SL:
